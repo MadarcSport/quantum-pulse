fda5cab4b107847940c3dc757a858506d4a8c3fa
Marc <marctoubia1985@gmail.com>
2026-07-02 02:24:34 +0800

    canva will work

---
+++ app/canva/CanvaApp2.d.ts
@@ -0,0 +1,8 @@
+import type { CSSProperties, ReactElement } from "react";
+
+export interface CanvaApp2Props {
+  style?: CSSProperties;
+  canvasStyle?: CSSProperties;
+}
+
+export default function CanvaApp2(props: CanvaApp2Props): ReactElement;
